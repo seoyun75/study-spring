f24d5c9a058419d5c31e3eb241b3a7c29b48daf8
seoyun75 <67564166+seoyun75@users.noreply.github.com>
2022-01-09 21:43:30 +0900

    learning GET API

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,24 +1,68 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.UserRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
 public class GetApiController {
 
-    @GetMapping("/hello")
-    String hello(){
-        System.out.println("Hello");
-        return "Hello";
+
+    @GetMapping("query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
+        //query paramiter를 받는 방법 1
+        //@RequestParam을 사용해 Map에 mappingg하여 사용
+
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+
+            sb.append(entry.getKey() + " = " + entry.getValue() + "\n");
+        });
+
+        return sb.toString();
     }
 
-    @RequestMapping(path = "/hi", method = RequestMethod.GET)
-    String hi(){
-        return "hi";
+    //query paramiter를 받는 방법 2
+    @GetMapping("query-param-2")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ) {
+
+        StringBuilder sb = new StringBuilder();
+
+
+        sb.append("name = " + name + "\n");
+        sb.append("email = " + email + "\n");
+        sb.append("email = " + age + "\n");
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return sb.toString();
     }
 
+    //쿼리파라미터의 변수와 매칭을 통해 값을 넣어줌
+    //query paramiter를 받는 방법 3
+    //dto 객체를 활용한 mappring
+    @GetMapping("query-param-3")
+    public String queryParam02(UserRequest userRequest) {//어노테이션을 사용하지않음
+
+        StringBuilder sb = new StringBuilder();
+
+
+        sb.append("name = " + userRequest.getName() + "\n");
+        sb.append("email = " + userRequest.getEmail() + "\n");
+        sb.append("email = " + userRequest.getAge() + "\n");
+        
+        return sb.toString();
+    }
 
 }
--- src/main/java/com/example/hello/dto/UserRequest.java
+++ src/main/java/com/example/hello/dto/UserRequest.java
@@ -1,2 +1,36 @@
-package com.example.hello.dto;public class UserRequest {
+package com.example.hello.dto;
+
+public class UserRequest {
+    /*
+    * parameter와 mapping할 DTO 객체 생성
+    * */
+
+
+    private String name;
+    private String email;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
